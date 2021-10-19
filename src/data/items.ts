787c9388dff66b89edce329526d57a5e5fd861f2
dyego kleodon <dyego.kleodon@gmail.com>
2021-10-19 15:34:36 -0300

    financeFull

---
+++ src/data/items.ts
@@ -0,0 +1,30 @@
+import {Item} from '../types/Item'
+
+export const items: Item[] = [
+  {
+    date: new Date(2021, 9, 15),
+    category: 'food',
+    title: 'Britos',
+    value: 15.50
+  },
+  {
+    date: new Date(2021, 9, 25),
+    category: 'rent',
+    title: 'Aluguel',
+    value: 260.51
+  },
+  {
+    date: new Date(2021, 10, 5),
+    category: 'food',
+    title: 'matterello',
+    value: 120
+  },
+  {
+    date: new Date(2021, 9, 1),
+    category: 'salary',
+    title: 'Salario UFPB',
+    value: 4296
+  },
+
+
+]
